81f735b9d343307de344085ba42f6481a377300b
yang83yang <masterd8483@126.com>
2022-10-02 10:13:00 +0900

    up

--- jdCookie.js
+++ jdCookie.js
@@ -3,7 +3,7 @@
  */
 //此处填写京东账号cookie。
 let CookieJDs = [
-  'pt_key=AAJjEvZuADCOuSn-OpRTTQ8Nmoq2QzEvLPjPQUvfOCqsYyvCv79PMgwOJBPprje-9J5yDVBYA1I;pt_pin=masterd8483;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJjOOVaADAD0nZYrzfw0N_70cZsq16COTVswtHeRWuFEcxhuI_S7awQ_piAKeNKTl4jhunS3Mo;pt_pin=masterd8483;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
   '',
 ]
