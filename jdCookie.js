296e4db85b45820231fb5682f765c55a9f13e5c0
yang83yang <masterd8483@126.com>
2024-02-21 21:55:17 +0800

    0221

--- jdCookie.js
+++ jdCookie.js
@@ -3,7 +3,7 @@
  */
 //此处填写京东账号cookie。
 let CookieJDs = [
-  'pt_key=AAJlzAo6ADDkfAnthU4RYGLOkigMHkAr6MLBpandB0S6p9gxVBKBYb0K2mieaGVaOMiLupHwI10;pt_pin=masterd8483;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJl1f9EADCqQT506OATd-IUmLerbNxmwSSi2Hbk9oNOlKi4Kmk2n-xQP5ir0pqFiuofuKHpHsU;pt_pin=masterd8483;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
   '',
 ]
